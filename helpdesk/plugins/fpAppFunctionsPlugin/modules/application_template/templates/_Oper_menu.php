ca24c03d25505a9cf1d8b2290e9193266ba14b1a
Pavel <ppecheny@i.ua>
2014-03-27 17:54:19 +0200

    #change pass

--- helpdesk/plugins/fpAppFunctionsPlugin/modules/application_template/templates/_Oper_menu.php
+++ helpdesk/plugins/fpAppFunctionsPlugin/modules/application_template/templates/_Oper_menu.php
@@ -28,3 +28,4 @@
         </a></li>
     <?php endif;?>
     <li><a href="http://intranet.griffin.ua"><?php echo __('Intranet') ?></a></li>
+    <li><a href="<?php echo url_for('changepass')?>" class="page_loader_link"><?php echo __('Change password') ?></a></li>
