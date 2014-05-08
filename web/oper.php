68d888828573a099767cf7667d35e54d4a05c136
Pavel <ppecheny@i.ua>
2014-05-08 14:05:07 +0300

    #arselor

--- web/oper.php
+++ web/oper.php
@@ -1,6 +1,6 @@
 <?php
 
-
+ini_set('memory_limit', '2047M');
 require_once(dirname(__FILE__).'/../helpdesk/config/ProjectConfiguration.class.php');
 
 //$configuration = ProjectConfiguration::getApplicationConfiguration('oper', 'dev', false);
