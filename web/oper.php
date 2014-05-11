519e809c94f16ad206c5e7656b2769416ad31cdf
Pavel <ppecheny@i.ua>
2014-05-11 23:49:47 +0300

    #299

--- web/oper.php
+++ web/oper.php
@@ -3,7 +3,7 @@
 ini_set('memory_limit', '2047M');
 require_once(dirname(__FILE__).'/../helpdesk/config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('oper', 'dev', false);
-//$configuration = ProjectConfiguration::getApplicationConfiguration('oper', 'dev', true);
+//$configuration = ProjectConfiguration::getApplicationConfiguration('oper', 'dev', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('oper', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
 
